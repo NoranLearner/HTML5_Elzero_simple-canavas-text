fa16525c536b4bac40ff55bec2fa71338e222e14
noranmostafa843@gmail.com <55337446+NoranLearner@users.noreply.github.com>
2021-01-23 00:39:46 +0200

    HTML5_Elzero_simple-canavas-text

---
+++ file.js
@@ -0,0 +1,64 @@
+var myCanvas = document.getElementById('c'),
+
+    myContext = myCanvas.getContext('2d');
+
+
+
+myContext.font = '60px Arial';
+
+myContext.fillStyle = '#080';
+
+myContext.fillText('E', 20, 50);
+
+
+
+
+myContext.font = '50px Arial';
+
+myContext.fillStyle = '#111';
+
+myContext.fillText('L', 40, 70);
+
+
+
+
+myContext.font = '40px Arial';
+
+myContext.fillStyle = '#353';
+
+myContext.fillText('Z', 60, 90);
+
+
+
+
+myContext.font = '30px Arial';
+
+myContext.fillStyle = '#777';
+
+myContext.fillText('E', 80, 110);
+
+
+
+
+myContext.font = '20px Arial';
+
+myContext.fillStyle = '#088';
+
+myContext.fillText('R', 100, 130);
+
+
+
+
+myContext.font = '10px Arial';
+
+myContext.fillStyle = '#880';
+
+myContext.fillText('O', 120, 150);
+
+
+
+myContext.font = '60px Arial';
+
+myContext.strokeStyle = 'brown';
+
+myContext.strokeText('School', 80, 220);
